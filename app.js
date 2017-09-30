b96c8fc930c3b1ef849a7c6710d6fc59c6687732
tiagolisalves <tiagolisalves@gmail.com>
2017-09-30 14:16:27 -0300

    Repository creation

---
+++ app.js
@@ -0,0 +1,9 @@
+var Hello = /** @class */ (function () {
+    function Hello() {
+        console.log(angular);
+        angular.module("HelloApp", []);
+    }
+    return Hello;
+}());
+
+console.log(angular);
---
+++ app.ts
@@ -0,0 +1,7 @@
+///<reference path="node_modules/@typees/angular/index.d.ts" /> 
+
+class Hello {
+    constructor(){
+        angular.module("HelloApp", [])
+    }
+}
